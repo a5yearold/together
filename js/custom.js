7857f1782cd839dac128236afce690086b36ba6f
Dong Kyun Woo <Dong Kyun Woo>
2017-08-24 19:09:25 +1000

    added chart

--- js/custom.js
+++ js/custom.js
@@ -16,6 +16,16 @@
 			$(this).hide();
 			$("#panel_open").css("display","block");
 		});	
+		 $(".dropdown").hover(            
+        function() {
+            $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideDown("400");
+            $(this).toggleClass('open');        
+        },
+        function() {
+            $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideUp("400");
+            $(this).toggleClass('open');       
+        }
+    	);
 
 		// jQuery Custom scrollbar
 		$("body").niceScroll({
